136ba1e8bd5aa29aab2f5abfe7c48f0c416f6270
Aaron Ward <aaromp@gmail.com>
2014-12-30 17:06:36 -0800

    add thumbnails to a container div

--- lightbox.js
+++ lightbox.js
@@ -2,6 +2,7 @@ var client_id = '38615c1e89344d13b07e194a36915fc8';
 var url = 'https://api.instagram.com/v1/tags/slack/media/recent?client_id=' + client_id;
 
 var thumbnails = [];
+var container;
 
 var appendThumbnails = function(json) {
 	var thumbnail;
@@ -10,11 +11,14 @@ var appendThumbnails = function(json) {
 		thumbnail.src = datum.images.thumbnail.url;
 		thumbnail.data = datum;
 		thumbnails.push(thumbnail);
-		document.body.appendChild(thumbnail);
+		container.appendChild(thumbnail);
 	});
 };
 
 document.addEventListener('DOMContentLoaded', function() {
+	container = document.createElement('div');
+	document.body.appendChild(container);
+
 	var script = document.createElement('script');
 	script.src = url + '&count=16' + '&callback=' + 'appendThumbnails';
 	document.body.appendChild(script);
