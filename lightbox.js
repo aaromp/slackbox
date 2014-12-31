8c87e0f40eaed6510adb059ce3b044edb4303424
Aaron Ward <aaromp@gmail.com>
2014-12-30 17:32:22 -0800

    add large image on thumbnail click

--- lightbox.js
+++ lightbox.js
@@ -2,10 +2,11 @@ var client_id = '38615c1e89344d13b07e194a36915fc8';
 var url = 'https://api.instagram.com/v1/tags/slack/media/recent?client_id=' + client_id;
 
 var thumbnails = [];
-var container;
+var container, image;
 
 var handleThumbnailClick = function(datum) {
-	console.log(datum.images.standard_resolution.url);
+	// console.log(datum.images.standard_resolution.url);
+	image.src = datum.images.standard_resolution.url;
 };
 
 var appendThumbnails = function(json) {
@@ -22,8 +23,11 @@ var appendThumbnails = function(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
 	container = document.createElement('div');
+	image = document.createElement('img');
 	document.body.appendChild(container);
+	container.appendChild(image);
 	container.id = 'container';
+	image.id = 'image';
 
 	var script = document.createElement('script');
 	script.src = url + '&count=16' + '&callback=' + 'appendThumbnails';
