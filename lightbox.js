019a4b4e91e7d8a9535b98fc98f59d663c79beb3
Aaron Ward <aaromp@gmail.com>
2014-12-31 12:12:50 -0800

    allow image to advance and return to its place

--- lightbox.js
+++ lightbox.js
@@ -1,8 +1,9 @@
 var client_id = '38615c1e89344d13b07e194a36915fc8';
 var url = 'https://api.instagram.com/v1/tags/iceland/media/recent?client_id=' + client_id;
 
+var data;
 var thumbnails = [];
-var container, overlays, image;
+var container, overlays, image, index;
 var states = {
 	initial: {},
 	final: {}
@@ -22,7 +23,8 @@ var setStyles = function(element, styles) {
 };
 
 var handleThumbnailClick = function(data) {
-	console.log(data);
+	console.log('thumbnail click');
+	index = this.index;
 	overlays.classList.add('active');
 	// initialize image
 	image.src = '';
@@ -37,19 +39,24 @@ var handleThumbnailClick = function(data) {
 		// set initial size and position
 		setState(states.initial, this, data.images.thumbnail.width/data.images.standard_resolution.width);
 		image.style.transition = 'none';
+		console.log(window.getComputedStyle(image).transition);
 		setStyles(image, states.initial);
 	
 		// set final size and position
 		setState(states.final, container, 1);
 		image.style.transition = 'all 0.5s ease';
 		setStyles(image, states.final);
+
+		image.onload = null; // remove onload function
 	}.bind(this);
 };
 
 var appendThumbnails = function(json) {
 	var thumbnail;
-	json.data.forEach(function(data) {
+	data = json.data;
+	json.data.forEach(function(data, index) {
 		thumbnail = document.createElement('img');
+		thumbnail.index = index;
 		thumbnail.classList.add('thumbnail');
 		thumbnail.src = data.images.thumbnail.url;
 		thumbnail.addEventListener('click', handleThumbnailClick.bind(thumbnail, data));
@@ -62,6 +69,15 @@ var createOverlay = function(position) {
 	var overlay = document.createElement('div');
 	overlay.id = position;
 	overlays.appendChild(overlay);
+	return overlay;
+};
+
+var handleOverlayClick = function(event) {
+	console.log(event.target.id, 'click');
+	if (event.target.id === 'right') index++;
+	image.style.transition = 'none';
+	setState(states.initial, thumbnails[index], data[index].images.thumbnail.width/data[index].images.standard_resolution.width);
+	image.src = data[index].images.standard_resolution.url;
 };
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -75,8 +91,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	container.id = 'container';
 	image.id = 'image';
 
-	createOverlay('left');
-	createOverlay('right');
+	createOverlay('left').addEventListener('click', handleOverlayClick);
+	createOverlay('right').addEventListener('click', handleOverlayClick);
 
 	var script = document.createElement('script');
 	script.src = url + '&count=16' + '&callback=' + 'appendThumbnails';
@@ -87,7 +103,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	container.appendChild(button);
 	button.style.position = 'absolute';
 	button.addEventListener('click', function(e) {
+		console.log('button click');
 		console.log(states, e);
+		image.style.transition = 'all 0.5s ease';
 		overlays.classList.remove('active');
 		
 		setStyles(image, states.initial);
