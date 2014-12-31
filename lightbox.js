df068d184ada584d57d2913ee3deade6c30026da
Aaron Ward <aaromp@gmail.com>
2014-12-30 17:02:10 -0800

    change callback to append thumbnails from instagram

--- lightbox.js
+++ lightbox.js
@@ -1,14 +1,18 @@
-var request;
-
 var client_id = '38615c1e89344d13b07e194a36915fc8';
-var url = 'https://api.instagram.com/v1/media/popular?client_id=' + client_id;
+var url = 'https://api.instagram.com/v1/tags/slack/media/recent?client_id=' + client_id;
 
-var callbackFunction = function(data) {
-	console.log('success', data);
+var appendThumbnails = function(json) {
+	var image;
+	json.data.forEach(function(datum) {
+		image = document.createElement('img');
+		image.src = datum.images.thumbnail.url;
+		image.data = datum;
+		document.body.appendChild(image);
+	});
 };
 
 document.addEventListener('DOMContentLoaded', function() {
 	var script = document.createElement('script');
-	script.src = url + '&callback=' + 'callbackFunction';
+	script.src = url + '&count=16' + '&callback=' + 'appendThumbnails';
 	document.body.appendChild(script);
 });
